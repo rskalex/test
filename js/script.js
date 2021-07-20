a84610d15ed2ea7e8eb6af1198d1dccb9bf1b3bd
Alex <rusak.alexander.94@gmail.com>
2021-07-20 12:05:25 +0300

    init: start test project

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+// console.log(user1);
+// console.log(user2);
+
+const user1 = 'Alex';
+console.log(user1);
+{
+    var user2 = 'Vlad';
+    // console.log(user2);
+    console.log(user1);
+}
+
+console.log(user1);
+
+// console.log(user2);
+
+let obj = {
+    a: 1,
+    b: 'a'
+};
+
+console.log(obj['a'])
