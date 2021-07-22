be98a7857da8b722976306f5127d8f2444398568
Alex <rusak.alexander.94@gmail.com>
2021-07-22 21:49:25 +0300

    init:start test project

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
-const numberOfFilms = +prompt('How many films have wached already?', '0'),
-      personalMoviesDB = {
-          count: numberOfFilms,
-          movies: {},
-          actors: {},
-          genres: [],
-          privat: false
-      };
+// const numberOfFilms = +prompt('How many films have wached already?', '0'),
+//       personalMoviesDB = {
+//           count: numberOfFilms,
+//           movies: {},
+//           actors: {},
+//           genres: [],
+//           privat: false
+//       };
 
-let a1 = prompt('One of the last wached films?', ''),
-    a2 = prompt('How much will his?', ''),
-    a3 = prompt('One of the last wached films?', ''),
-    a4 = prompt('How much will his?', '');
+// let a1 = prompt('One of the last wached films?', ''),
+//     a2 = prompt('How much will his?', ''),
+//     a3 = prompt('One of the last wached films?', ''),
+//     a4 = prompt('How much will his?', '');
 
-personalMoviesDB.movies[a1] = a2;
-personalMoviesDB.movies[a3] = a4;
+// personalMoviesDB.movies[a1] = a2;
+// personalMoviesDB.movies[a3] = a4;
 
-console.log(personalMoviesDB);
+// console.log(personalMoviesDB);
+
+
+let n = 10;
+a:
+for (let i = 2; i <= n; i++) {
+    for(let j = 2; j < i; j++) {
+        if(i % j == 0) {
+            continue a;
+        }
+    }
+    console.log(i)
+}
+
+
